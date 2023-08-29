2d3ae01ab9088288a74b2fa2e0ddbfcaa94aa3c7
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-08-29 17:29:28 -0300

    Sidebar - Estrutura do layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      maxWidth: {
-        app: '700px'
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
       },
+
+      maxWidth: {
+        app: '700px',
+      },
+
       colors: {
-        fellipe: '#333'
-      }
+        fellipe: '#333',
+      },
     },
   },
   plugins: [],
