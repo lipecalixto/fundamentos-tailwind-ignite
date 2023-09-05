dc524bd3e99006abc39372a81e05b69f4e19adf1
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-09-05 18:55:40 -0300

    Responsividade e dark mode - Tema dark da sidebar

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -10,10 +10,10 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Fellipe Calixto
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           fellipe.calixto.santos@gmail.com
         </span>
       </div>
